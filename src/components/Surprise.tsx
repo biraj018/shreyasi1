295488b5c01b7675e0f72c4e8fac11a56e2259b9
biraj018 <sen.birajsen@gmail.com>
2025-12-03 22:53:57 +0530

    Update Surprise.tsx

--- src/components/Surprise.tsx
+++ src/components/Surprise.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import { Music, Cake as CakeIcon } from 'lucide-react';
 import Banner from './images/banner.png';
-import Musicc from 'src/components/musicc.mp3';
+import Musicc from 'musicc.mp3';
 import CatCake from './images/cat-face-cake.jpg'
 const Surprise = () => {
   const [stage, setStage] = useState(0);
@@ -153,6 +153,7 @@ const Surprise = () => {
 };
 
 export default Surprise;
+
 
 
 
